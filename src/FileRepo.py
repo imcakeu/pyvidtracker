73526a1a2a4fc27013eb9f04f444c4fdecf7d118
Eduard Diconi <eduard.diconi@etu.u-bordeaux.fr>
2024-02-20 10:30:39 +0100

    scripts py

---
+++ src/FileRepo.py
@@ -0,0 +1,17 @@
+import csv
+from Point import *
+
+def CSVExport(points, path):
+    with open(path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["X", "Y"])
+        for point in points:
+            writer.writerow([point.get_x(), point.get_y()])
+
+
+point = Point(12, 34)
+point2 = Point(56, 78)
+point3 = Point(90, 0)
+
+myPoints = [point, point2, point3]
+CSVExport(myPoints, "./fichier.csv")
---
+++ src/Point.py
@@ -0,0 +1,10 @@
+class Point:
+    def __init__(self, x, y):
+        self.__x = x  # private variable for X
+        self.__y = y  # private variable for Y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
