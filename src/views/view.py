988ea254fcbe9d722478d53a949c20b9cfd146fa
Eduard Diconi <eduard.diconi@etu.u-bordeaux.fr>
2024-03-05 09:58:19 +0100

    Menu deroulant

--- src/views/view.py
+++ src/views/view.py
@@ -1,15 +1,40 @@
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
-from tkinter import ttk
+from tkinter import Tk, Frame, Menu
 
 class View(tk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
-        
-    
+
+        self.initUI()
+
     def setController(self, controller):
         self.controller = controller
 
+    def initUI(self):
+        self.master.title("Simple menu")
 
-    
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label="Charger video")
+        fileMenu.add_command(label="Lire video")
+        fileMenu.add_command(label="Quitter", command=self.onExit)
+        menubar.add_cascade(label="Fichier", menu=fileMenu)
+
+        scaleMenu = Menu(menubar)
+        scaleMenu.add_command(label="Creer une nouvelle Echelle")
+        scaleMenu.add_command(label="Activer l'affichage de l'Echelle")
+        menubar.add_cascade(label="Echelle", menu=scaleMenu)
+
+
+    def onExit(self):
+        self.quit()
+
+def main():
+    root = tk()
+    root.geometry("250x150+300+300")
+    app = View()
+    root.mainloop()
