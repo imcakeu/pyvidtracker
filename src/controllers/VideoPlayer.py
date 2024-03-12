5ea5dd6f893bd72967b4596a12d74a16d50e1880
Eduard Diconi <arkadiastudioinc@gmail.com>
2024-03-12 09:18:49 +0100

    VideoPlayer.py

--- src/controllers/VideoPlayer.py
+++ src/controllers/VideoPlayer.py
@@ -2,12 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 import PIL.Image, PIL.ImageTk
 import cv2
-
+import os
 
 class Application:
-
     def __init__(self, window, window_title):
-
         self.window = window
         self.window.title(window_title)
         self.canvas = Canvas(window)
@@ -16,28 +14,28 @@ class Application:
         self.open_file()
         self.play_video()
         self.window.mainloop()
-        
 
+    def get_video_file(self, video_name):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        video_path = os.path.abspath(os.path.join(script_dir, '..', '..', 'resources', 'videos', video_name))
+        return video_path
 
     def open_file(self):
         self.pause = False
-        self.filename = "path/to/video"
+        self.filename = self.get_video_file("compteur.mp4")
         print(self.filename)
         self.cap = cv2.VideoCapture(self.filename)
         self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.canvas.config(width = self.width, height = self.height)
 
-
-    # get only one frame    
-    def get_frame(self):   
+    def get_frame(self):
         try:
             if self.cap.isOpened():
                 ret, frame = self.cap.read()
                 return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         except:
             messagebox.showerror(title='Alert', message='End of the video.')
-
 
     def play_video(self):
         # Get a frame from the video source, and go to the next frame automatically
@@ -48,8 +46,6 @@ class Application:
         if not self.pause:
             self.window.after(self.delay, self.play_video)
 
-
-    # Release the video source when the object is destroyed
     def __del__(self):
         if self.cap.isOpened():
             self.cap.release()
