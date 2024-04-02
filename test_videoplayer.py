2ef30a6d478363c1c0686048a80917d533c9aca8
Edward Diconi <arkadiastudioinc@gmail.com>
2024-04-02 09:50:03 +0200

    test

---
+++ test_videoplayer.py
@@ -0,0 +1,20 @@
+import unittest
+
+class test_videoplayer(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_readColCSVwithoutComment(self):
+        pass
+
+    def test_readColCSVwithComment(self):
+        pass
+
+    def test_numberElementInColumnWithoutComment(self):
+        pass
+
+    def test_numberElementInColumnWithComment(self):
+        pass
