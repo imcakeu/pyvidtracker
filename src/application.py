27c10c0aa31dc8c0962caa33ac4b6e17917808c3
Edward Diconi <arkadiastudioinc@gmail.com>
2024-04-16 08:30:58 +0200

    Enfin fonctionnel

--- src/application.py
+++ src/application.py
@@ -3,21 +3,20 @@ from controllers.controller import Controller
 from controllers.videoPlayer import VideoPlayer
 from views.view import View
 class Application(tk.Tk):
-    def __init__(self):
+    def __init__(self, file_name="default"):
         super().__init__()
         self.title('VideoTracker Media Player')
         self.geometry("640x400+300+300")
         self.resizable(False, False)
 
         self.view = View(self)
-        self.controller = Controller(self, self.view, "default")
+        self.controller = Controller(self, self.view, file_name)
 
     def new_player(self, file_name):
+        self.controller.videoPlayer.canvas.delete('all')
         self.destroy()  # Destroy the main application window
-        self.pack_forget()
-        self.grid_forget()
-        self.__init__()  # Recreate the application with a new instance
-        self.controller = Controller(self, self.view, file_name)
+        self.__init__(file_name)  # Recreate the application with a new instance
+        # self.controller = Controller(self, self.view, file_name)
 
 
 if __name__ == '__main__':
